47b4470a443be10bb128f5495f6111df252ca519
Ryan Weaver <ryan@thatsquality.com>
2014-02-07 11:02:04 -0500

    [tuts-apply-diff] Applying existing diff file for step build-completing-crud-controller

--- src/App/Controller/TeamController.php
+++ src/App/Controller/TeamController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Team;
+use Knp\RadBundle\Controller\Controller;
 
 class TeamController extends Controller
 {
@@ -15,5 +15,60 @@ class TeamController extends Controller
     public function indexAction(array $teams)
     {
         return array('teams' => $teams);
+    }
+
+    /**
+     * @param Team $team
+     * @return array
+     */
+    public function showAction(Team $team)
+    {
+        return array('team' => $team);
+    }
+
+    public function editAction(Team $team)
+    {
+        $form = $this->createBoundObjectForm($team, 'edit', array(
+            'method' => 'PUT',
+            'action' => $this->generateUrl('app_team_update', array('id' => $team->getId()))
+        ));
+
+        if ($form->isValid()) {
+            $this->persist($team, true);
+            $this->addFlash('success');
+
+            return $this->redirectToRoute('app_team_show', array(
+                'id' => $team->getId()
+            ));
+        }
+
+        return ['form' => $form->createView(), 'team' => $team];
+    }
+
+    public function newAction()
+    {
+        $team = new Team();
+        $form = $this->createBoundObjectForm($team, 'new', array(
+            'method' => 'POST',
+            'action' => $this->generateUrl('app_team_create', array('id' => $team->getId()))
+        ));
+
+        if ($form->isValid()) {
+            $this->persist($team, true);
+            $this->addFlash('success');
+
+            return $this->redirectToRoute('app_team_show', array(
+                'id' => $team->getId()
+            ));
+        }
+
+        return ['form' => $form->createView(), 'team' => $team];
+    }
+
+    public function deleteAction(Team $team)
+    {
+        $this->remove($team, true);
+
+        return $this->redirectToRoute('app_team_index');
     }
 }
